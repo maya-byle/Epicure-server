e62fff3d02d597abfce04e8d049422ebd94de40d
mayabyle <mayab@moveo.com>
2024-02-01 16:53:08 +0200

    Added try/catch block for error handling

--- handlers/search.handler.ts
+++ handlers/search.handler.ts
@@ -4,32 +4,37 @@ import restaurantModel, { IRestaurant } from "../models/restaurant.model";
 import chefModel, { IChef } from "../models/chef.model";
 
 const get = async (searchInput: string) => {
-  const restaurants: IRestaurant[] = await restaurantModel
-    .find({
-      title: { $regex: searchInput, $options: "i" },
-      status: DeleteStatus.ACTIVE,
-    })
-    .lean();
+  try {
+    const restaurants: IRestaurant[] = await restaurantModel
+      .find({
+        title: { $regex: searchInput, $options: "i" },
+        status: DeleteStatus.ACTIVE,
+      })
+      .lean();
 
-  const chefs: IChef[] = await chefModel
-    .find({
-      name: { $regex: searchInput, $options: "i" },
-      status: DeleteStatus.ACTIVE,
-    })
-    .lean();
+    const chefs: IChef[] = await chefModel
+      .find({
+        name: { $regex: searchInput, $options: "i" },
+        status: DeleteStatus.ACTIVE,
+      })
+      .lean();
 
-  const dishes: IDish[] = await dishModel
-    .find({
-      name: { $regex: searchInput, $options: "i" },
-      status: DeleteStatus.ACTIVE,
-    })
-    .lean();
+    const dishes: IDish[] = await dishModel
+      .find({
+        name: { $regex: searchInput, $options: "i" },
+        status: DeleteStatus.ACTIVE,
+      })
+      .lean();
 
-  return {
-    restaurants: restaurants,
-    chefs: chefs,
-    dishes: dishes,
-  };
+    return {
+      restaurants: restaurants,
+      chefs: chefs,
+      dishes: dishes,
+    };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
 
 export default { get };
