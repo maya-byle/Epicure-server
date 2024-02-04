2317b0cd6c2ec5244d1341d5f5732f146a8ddc88
mayabyle <mayab@moveo.com>
2024-02-04 11:47:04 +0200

    improve search efficiency with Promise.all

--- handlers/search.handler.ts
+++ handlers/search.handler.ts
@@ -5,31 +5,31 @@ import chefModel, { IChef } from "../models/chef.model";
 
 const searchAll = async (searchInput: string) => {
   try {
-    const restaurants: IRestaurant[] = await restaurantModel
-      .find({
-        title: { $regex: searchInput, $options: "i" },
-        status: DeleteStatus.ACTIVE,
-      })
-      .lean();
-
-    const chefs: IChef[] = await chefModel
-      .find({
-        name: { $regex: searchInput, $options: "i" },
-        status: DeleteStatus.ACTIVE,
-      })
-      .lean();
-
-    const dishes: IDish[] = await dishModel
-      .find({
-        name: { $regex: searchInput, $options: "i" },
-        status: DeleteStatus.ACTIVE,
-      })
-      .lean();
+    const [restaurants, chefs, dishes] = await Promise.all([
+      restaurantModel
+        .find({
+          title: { $regex: searchInput, $options: "i" },
+          status: DeleteStatus.ACTIVE,
+        })
+        .lean(),
+      chefModel
+        .find({
+          name: { $regex: searchInput, $options: "i" },
+          status: DeleteStatus.ACTIVE,
+        })
+        .lean(),
+      dishModel
+        .find({
+          name: { $regex: searchInput, $options: "i" },
+          status: DeleteStatus.ACTIVE,
+        })
+        .lean(),
+    ]);
 
     return {
-      restaurants: restaurants,
-      chefs: chefs,
-      dishes: dishes,
+      restaurants,
+      chefs,
+      dishes,
     };
   } catch (err) {
     console.log(err);
