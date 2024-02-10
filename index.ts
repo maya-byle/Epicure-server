c8327f54d9aabe15cb336c1a55e42399989d45c6
mayabyle <mayab@moveo.com>
2024-02-10 22:01:05 +0200

    fixed routes, handlers, controllera to match generic client requests

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 import { connectToDb } from "./db";
 import apiRoutes from "./routes/api/api.route";
 import swaggerRoutes from "./utils/swagger";
+import adminRoutes from "./routes/admin/admin.route";
 
 const app: express.Application = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/api", apiRoutes);
+app.use("/admin", adminRoutes);
 app.use("/api-docs", swaggerRoutes);
 
 function startServer(): void {
